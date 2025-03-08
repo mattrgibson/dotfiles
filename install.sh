0e9d624227aa1e7f92ce862faf00c6053ba4c8f4
Matt Gibson <74279237+mattrgibson@users.noreply.github.com>
2025-03-08 15:12:24 -0500

    updated macos config

--- install.sh
+++ install.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 echo "Installing Vim Monk System..."
 
-# Borrowed this setup from https://github.com/mischavandenburg/dotfiles/blob/main/setup
-
-# create directories
-export XDG_CONFIG_HOME="$HOME"/.dotfiles
-mkdir -p "$XDG_CONFIG_HOME"/zsh
-mkdir -p "$XDG_CONFIG_HOME"/alacritty
-
-mkdir -p "$XDG_CONFIG_HOME"/nvim
-mkdir -p "$XDG_CONFIG_HOME"/starship
-
-
 # Symlink dotfiles
-ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
-ln -sf ~/.dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-ln -sf ~/.dotfiles/nvim/init.lua ~/.config/nvim/init.lua
+ln -sf ~/.config/zsh/.zshrc ~/.zshrc
 
 # Install all Brew packages
-brew bundle install --file ~/.dotfiles/Brewfile
+brew bundle install --file ~/.config/Brewfile
 
 echo "Welcome back, monk."
