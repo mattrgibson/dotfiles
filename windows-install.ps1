68f3a4c542e379293631902b688f5695bf01b6e0
Matt Gibson <74279237+mattrgibson@users.noreply.github.com>
2025-03-08 14:47:45 -0500

    windows config install

---
+++ windows-install.ps1
@@ -0,0 +1,24 @@
+#Requires -RunAsAdministrator
+
+# Install software packages
+winget install --id vscode --source winget --accept-source-agreements --silent
+winget install --id Git.Git -e --source winget --accept-source-agreements --silent
+winget install --id Starship.Starship --accept-source-agreements --silent
+winget install --id Neovim.Neovim --accept-source-agreements --silent
+winget install --exact --id Microsoft.AzureCLI --accept-source-agreements --silent
+winget install --id Microsoft.Powershell --source winget --accept-source-agreements --silent
+winget install --id OpenWhisperSystems.Signal --source winget --accept-source-agreements --silent
+winget install --id Doist.Todoist --source winget --accept-source-agreements --silent
+winget install --id Obsidian.Obsidian --source winget --accept-source-agreements --silent
+winget install --id mRemoteNG.mRemoteNG --source winget --accept-source-agreements --silent
+winget install --id Apple.iCloud --source winget --accept-source-agreements --silent
+
+# Create a symlink to our config directory and PowerShell profile
+$linkLocation = ""
+if ($PSVersionTable.PSVersion.Major -ge 7) {
+	$linkLocation = "~\.config\powershell\profile7.ps1"
+}
+else {	
+	$linkLocation = "~\.config\powershell\profile5.ps1"
+}
+New-Item -Path $linkLocation -ItemType SymbolicLink -Target $PROFILE -Force
